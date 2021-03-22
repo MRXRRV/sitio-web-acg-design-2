546633fa9c69bdb79ef58e14374489ca613db9a6
rafael <rrv9606infofesc@gmail.com>
2021-03-22 12:12:02 -0600

    se agrego otro ahref mas en el menu x2

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
 					<a href="#">inicio</a>
 					<a href="#">inicio</a>
 					<a href="#">inicio</a>
+					<a href="#">inicio</a>
 				</div>
 				<div class="hamburger">
 					<i class="fas fa-bars"></i>
